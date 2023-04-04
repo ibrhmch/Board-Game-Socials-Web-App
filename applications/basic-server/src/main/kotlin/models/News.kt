21940fa78cfdbc74ff977fa153de0bc3b11399f8
Khaled <khaledhossain.not@gmail.com>
2023-04-04 12:54:24 -0600

    Added api

---
+++ applications/basic-server/src/main/kotlin/models/News.kt
@@ -0,0 +1,26 @@
+package models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Articles(
+    val source: Source?,
+    val author: String?,
+    val title: String?,
+    val description: String?,
+    val url: String?,
+    val urlToImage: String?,
+    val publishedAt: String?,
+    val content: String?
+)
+@Serializable
+data class Source(
+    val id: String?,
+    val name: String?
+)
+
+@Serializable
+data class NewsResponse(val status: String?, val totalResults: Int?, val articles: List<Articles>?)
+
+class News
+private constructor(articles: List<Articles>?)
