7c8210d6d34ef631c58b31b7749678e38c0c04af
Abhishek Purushothama <abhishek.purushothama@colorado.edu>
2023-04-11 20:14:34 -0600

    Refactoring for some re-use

--- applications/basic-server/src/test/kotlin/test/goodboards/app/PostFormTest.kt
+++ applications/basic-server/src/test/kotlin/test/goodboards/app/PostFormTest.kt
@@ -1,24 +1,24 @@
 package test.goodboards.app;
 
-import io.ktor.client.request.forms.*
 import io.ktor.http.*
-import io.ktor.request.*
 import io.ktor.server.testing.*
-import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class PostFormTest: BaseAppTest() {
 
+
     @Test
     fun testPutFormRespondsWithName() = testApp {
+        val post_param_name : String = "title"
+        val post_param_value : String = "ABCD"
         handleRequest(HttpMethod.Post, "/form") {
             addHeader(HttpHeaders.ContentType, ContentType.Application.FormUrlEncoded.toString())
-            setBody(listOf("title" to "ABCD").formUrlEncode())
+            setBody(listOf(post_param_name to post_param_value).formUrlEncode())
         }.apply{
                 assertEquals(200, response.status()?.value)
-                assertTrue(response.content!!.contains("ABCD"))
+                assertTrue(response.content!!.contains(post_param_value))
         }
     }
 
