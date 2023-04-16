d0a1e97bf55602c782b5308c94e7eebca65de5c8
Abhishek Purushothama <abhishek.purushothama@colorado.edu>
2023-04-16 16:01:02 -0600

    Base skeleton

---
+++ integration-tests/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -0,0 +1,13 @@
+package io.cucumber.skeleton;
+
+import io.cucumber.java.en.Given;
+
+public class StepDefinitions {
+    @Given("Form page should have the top level tex ")
+    public void getFormPageAndCheckTopLevelText() {
+        // ApiClient initialization
+        // Make the call
+        // Get the HTML response
+        // Parse and find the element.
+    }
+}
