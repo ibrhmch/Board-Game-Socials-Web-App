fe51f53c843c3d2ee3bf4e4d654bd2141a055454
TuanTran-MNS <tmatuan96@gmail.com>
2023-04-16 16:13:03 -0600

    initial commit

--- applications/basic-server/src/main/kotlin/com/goodboards/app/App.kt
+++ applications/basic-server/src/main/kotlin/com/goodboards/app/App.kt
@@ -13,6 +13,9 @@ import io.ktor.util.*
 import io.ktor.util.pipeline.*
 import models.Game
 import java.util.*
+import java.io.File
+import java.sql.DriverManager
+import com.fasterxml.jackson.databind.ObjectMapper
 
 val games = mutableListOf(
     Game.newEntry("Uno", "typical friendship destroying game"),
@@ -56,8 +59,40 @@ private fun PipelineContext<Unit, ApplicationCall>.headers(): MutableMap<String,
     return headers
 }
 
+private fun readGameJsonIntoDB(jsonFilePath: String, url: String, user: String, password: String): Boolean{
+    val url = "postgres://oqvckqlhqqiscz:196dc00d1eb1a6628992e3652ad2fcce8842a56086d4b4e1cb918007c00dc338@ec2-44-215-40-87.compute-1.amazonaws.com:5432/dbasdbm3uauplc"
+    val user = "oqvckqlhqqiscz"
+    val password = "196dc00d1eb1a6628992e3652ad2fcce8842a56086d4b4e1cb918007c00dc338"
+    val tableName = "Games"
+
+    // Connect to the Heroku database
+    DriverManager.getConnection(url, user, password).use { conn ->
+        conn.createStatement().use { stmt ->
+
+            // Open and read the JSON file
+            val jsonString = File(jsonFilePath).readText()
+
+            // Parse the JSON data
+            val objectMapper = ObjectMapper()
+            val jsonNode = objectMapper.readTree(jsonString)
+
+            // Insert the JSON data into the database
+            val sql = "INSERT INTO $tableName (json_data) VALUES (?)"
+            val pstmt = conn.prepareStatement(sql)
+            pstmt.setObject(1, jsonNode)
+            pstmt.executeUpdate()
+
+            // Close the prepared statement
+            pstmt.close()
+        }
+    }
+
+    return true;
+}
+
 fun main() {
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     val port = System.getenv("PORT")?.toInt() ?: 8888
+    readGameJsonIntoDB("game_info.json");
     embeddedServer(Netty, port, watchPaths = listOf("basic-server"), module = { module() }).start()
 }
