8b59ea66cb81087841e6d5a53378bfa070383557
Moppit <michelletranusa@gmail.com>
2023-04-18 23:28:51 -0600

    Unit tests: getAllGames() happy path

--- components/db-interface/src/test/kotlin/test/goodboards/db/DBInterfaceTest.kt
+++ components/db-interface/src/test/kotlin/test/goodboards/db/DBInterfaceTest.kt
@@ -1,16 +1,19 @@
 package test.goodboards.db
 
+import com.goodboards.db.Game
 import com.goodboards.db.DBInterface
 import com.goodboards.db.DBConnection
 import org.junit.Test
 import kotlin.test.*
 import io.mockk.*
 import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 
 class DBInterfaceTest {
 
     @Test
-    fun testIsConnected_HappyPath() {
+    fun testIsConnected_ConnectionExists() {
         // given -> setup
         val mockedConnection: Connection = mockk(relaxed = true)
         every { mockedConnection.isValid(0) } returns true
@@ -39,6 +42,43 @@ class DBInterfaceTest {
 
         // then -> verify results
         assertFalse { status }
+    }
+
+    @Test
+    fun testGetAllGames_Multiple() {
+        // given -> setup
+        // *** Setup desired game values
+        val games = mutableListOf(
+            Game("1", "Chess", "Classic Chess"),
+            Game("2", "Uno", "Classic Uno")
+        )
+        val helper = DBInterfaceTestHelpers()
+        helper.parseGames(games)
+        val ids = helper.getGameIds()
+        val names = helper.getGameNames()
+        val descriptions = helper.getGameDescriptions()
+        val iterations = helper.getGameIterations()
+        // *** Mock fetched games
+        val mockedResultSet: ResultSet = mockk(relaxed = true)
+        every { mockedResultSet.getString("id") } returnsMany ids
+        every { mockedResultSet.getString("name") } returnsMany names
+        every { mockedResultSet.getString("description") } returnsMany descriptions
+        every { mockedResultSet.next() } returnsMany iterations
+        // *** Mock query process
+        val mockedStatement: PreparedStatement = mockk(relaxed = true)
+        every { mockedStatement.executeQuery() } returns mockedResultSet
+        // *** Mock DB Connection
+        val mockedConnection: Connection = mockk(relaxed = true)
+        every { mockedConnection.prepareStatement("SELECT * FROM goodboards.games;") } returns mockedStatement
+        val mockedDBConnection: DBConnection = mockk(relaxed = true)
+        every { mockedDBConnection.getConnection() } returns mockedConnection
+
+        // when -> test action
+        val dbInterface = DBInterface(mockedDBConnection)
+        val result = dbInterface.getAllGames()
+
+        // then -> verify results
+        assertEquals(games, result)
     }
 
     /*  Things to Test
