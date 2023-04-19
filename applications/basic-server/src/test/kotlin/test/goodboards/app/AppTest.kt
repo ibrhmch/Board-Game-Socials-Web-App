43eb375f14ec55f132325999a3c955dd5bdf9202
Lin Shi <linshi1768@gmail.com>
2023-04-19 14:28:36 -0600

    include tests

--- applications/basic-server/src/test/kotlin/test/goodboards/app/AppTest.kt
+++ applications/basic-server/src/test/kotlin/test/goodboards/app/AppTest.kt
@@ -22,18 +22,18 @@ class AppTest : BaseAppTest() {
     @Test
     fun testContactPage() = testApp {
         handleRequest(HttpMethod.Get, "/").apply {
+    fun testEmptyHome() = testApp {
+        handleRequest(HttpMethod.Get, "").apply {
             assertEquals(200, response.status()?.value)
             assertTrue(response.content!!.contains("GOODBOARDS"))
         }
     }
-
     @Test
-    fun singleGame() = testApp {
-        handleRequest(HttpMethod.Get, "/game/0").apply {
+    fun testContactPage() = testApp {
+        handleRequest(HttpMethod.Get, "/").apply {
             assertEquals(200, response.status()?.value)
             // make sure news is part of the game display
             assertTrue(response.content!!.contains("News"))
-            assertTrue(response.content!!.contains("news"))
 
             assertTrue(response.content!!.contains("Contact"))
         }
