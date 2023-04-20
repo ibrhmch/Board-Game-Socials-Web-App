b6e1088485aac3cbfe80bc99e8f3015dcb3443b1
Khaled <khaledhossain.not@gmail.com>
2023-04-19 21:24:31 -0600

    Added test for getting games

--- applications/basic-server/src/test/kotlin/test/goodboards/app/GetFormTest.kt
+++ applications/basic-server/src/test/kotlin/test/goodboards/app/GetFormTest.kt
@@ -2,13 +2,13 @@ package test.goodboards.app
 
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import models.Game
 import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class GetFormTest : BaseAppTest() {
-
 
     @Test
     fun testGetFormRespondsWithForm() = testApp {
@@ -17,4 +17,16 @@ class GetFormTest : BaseAppTest() {
             assertTrue(response.content!!.contains("GOODBOARDS"))
         }
     }
+
+    @Test
+    fun testGetData() = testApp {
+        handleRequest(HttpMethod.Get, "/").apply {
+            val content = response.content!!;
+            val expectedNumberOfGames = 2;
+            val pattern  = "(Uno)".toRegex()
+            val found = pattern.find(content)!!.groupValues
+            assertEquals(found.size, expectedNumberOfGames)
+        }
+    }
+
 }
