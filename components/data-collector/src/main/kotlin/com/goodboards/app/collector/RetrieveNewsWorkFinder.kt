9b798364d58df2dccac28109c527909a918d8f59
Moppit <michelletranusa@gmail.com>
2023-04-26 13:01:22 -0600

    New worker type, still need functionality

--- components/data-collector/src/main/kotlin/com/goodboards/app/collector/RetrieveNewsWorkFinder.kt
+++ components/data-collector/src/main/kotlin/com/goodboards/app/collector/RetrieveNewsWorkFinder.kt
@@ -3,18 +3,18 @@ package com.goodboards.app.collector
 import com.goodboards.workflow.WorkFinder
 import org.slf4j.LoggerFactory
 
-class ExampleWorkFinder : WorkFinder<ExampleTask> {
+class RetrieveNewsWorkFinder : WorkFinder<RetrieveNewsTask> {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    override fun findRequested(name: String): List<ExampleTask> {
+    override fun findRequested(name: String): List<RetrieveNewsTask> {
         logger.info("finding work.")
 
-        val work = ExampleTask("some info")
+        val work = RetrieveNewsTask("some info")
 
         return mutableListOf(work)
     }
 
-    override fun markCompleted(info: ExampleTask) {
+    override fun markCompleted(info: RetrieveNewsTask) {
         logger.info("marking work complete.")
     }
 }
