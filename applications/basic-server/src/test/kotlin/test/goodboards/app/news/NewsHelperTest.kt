4617b83a4316dd762cb86e82e2c6d44a6c9ed3e8
Khaled <khaledhossain.not@gmail.com>
2023-04-28 20:35:51 -0600

    added 1 test

---
+++ applications/basic-server/src/test/kotlin/test/goodboards/app/news/NewsHelperTest.kt
@@ -0,0 +1,23 @@
+package test.goodboards.app.news
+
+import com.goodboards.app.news.News
+import com.goodboards.app.news.NewsHelper
+import io.mockk.every
+import io.mockk.verify
+import org.junit.Test
+import test.goodboards.app.util.DBMock
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class NewsHelperTest {
+    @Test
+    fun testGetNewsForGame() {
+        DBMock.mockDBConnection()
+        val dbInterface = DBMock.mockDBInterface()
+        val dbNews = DBMock.mockNews()
+        every { dbInterface.getNewsForGame("Uno") } returns dbNews
+        val news: List<News> = NewsHelper.getNewsForGame("Uno");
+        verify (exactly = 1) { dbInterface.getNewsForGame("Uno") }
+        assertTrue { dbNews.size == news.size }
+    }
+}
