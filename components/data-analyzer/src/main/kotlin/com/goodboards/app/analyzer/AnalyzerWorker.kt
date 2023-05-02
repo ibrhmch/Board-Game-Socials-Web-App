e7974e200f836f2684c9a60b249a81cc7ebcc2e6
Lin Shi <linshi1768@gmail.com>
2023-05-01 20:37:35 -0600

    UT tests (not done)

--- components/data-analyzer/src/main/kotlin/com/goodboards/app/analyzer/AnalyzerWorker.kt
+++ components/data-analyzer/src/main/kotlin/com/goodboards/app/analyzer/AnalyzerWorker.kt
@@ -4,49 +4,9 @@ import com.goodboards.db.News
 import com.goodboards.workflow.Worker
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
-import java.sql.Connection
-import java.sql.DriverManager
-import java.util.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
-object ConnectionHelper{
-    fun getConnection(): Connection {
-        val databaseCredential = DatabaseInit.getDatabaseCredentials()
-        return DriverManager.getConnection(databaseCredential.url, databaseCredential.username, databaseCredential.password)
-    }
-}
-object DatabaseInit{
-    const val NEWS_TABLE_NAME: String = "goodboards.news"
-    data class DatabaseCredential(val url: String, val username:String, val password: String)
-    fun getDatabaseCredentials() : DatabaseCredential  {
-        val DB_URL: String = EnvHelper.getEnv("JDBC_DATABASE_URL")
-        val DB_USENAME : String =  EnvHelper.getEnv("DATABASE_USERNAME")
-        val DB_PASSWORD : String = EnvHelper.getEnv("DATABASE_PASSWORD")
-        return DatabaseCredential(DB_URL, DB_USENAME, DB_PASSWORD)
-    }
-    fun getInsertNewsStatement(id: String, news: NewsUnit): String {
-        return "INSERT INTO $NEWS_TABLE_NAME(id, gameId, title, description, url) VALUES ('${id}', '${news.gameID}', '${news.title}', '${news.description}', '${news.url}');"
-    }
-
-    fun setDoubleApostrophe(str: String): String{
-        var temp = ""
-        for(character in str.iterator()){
-            if(character == '\''){
-                temp = temp + character.toString() + character.toString()
-            }else {
-                temp += character.toString()
-            }
-        }
-        return temp
-    }
-}
-
-object EnvHelper {
-    fun getEnv(key: String): String = System.getenv(key)!!
-}
-
-object UUIDHelper{
-    fun randomUUID(): UUID = UUID.randomUUID()!!
-}
 
 class ExampleWorker(override val name: String = "data-analyzer") : Worker<AnalyzerTask> {
     private val logger = LoggerFactory.getLogger(this.javaClass)
@@ -56,19 +16,19 @@ class ExampleWorker(override val name: String = "data-analyzer") : Worker<Analyz
             logger.info("starting data analysis.")
 
             // todo - data analysis happens here
-            val conn = ConnectionHelper.getConnection()!!
-            val redisInterface = RedisInterface()
+            val conn = Wrapper.getDBConnection()
+            val redisInterface = Wrapper.getRedisInterface()
             val redisQueue = redisInterface.getFromList(key = "news:collect-analyze", 10)
             for (item in redisQueue) {
                 var newsItem = Json.decodeFromString<NewsUnit>(item)
-                newsItem.title = DatabaseInit.setDoubleApostrophe(newsItem.title)
-                newsItem.description = DatabaseInit.setDoubleApostrophe(newsItem.description)
+                newsItem.title = Wrapper.setDoubleApostrophe(newsItem.title)
+                newsItem.description = Wrapper.setDoubleApostrophe(newsItem.description)
                 val query =
-                    conn.prepareStatement("SELECT * FROM ${DatabaseInit.NEWS_TABLE_NAME} where title = '${newsItem.title}';")
+                    conn.prepareStatement("SELECT * FROM ${Wrapper.getTableName()} where title = '${newsItem.title}';")
                 val result = query.executeQuery()
                 if (!result.next()) {
                     val newsInsertStatement =
-                        DatabaseInit.getInsertNewsStatement(UUIDHelper.randomUUID().toString(), newsItem)
+                        Wrapper.getInsertNewsStatement(Wrapper.getRandomUUID(), newsItem)
                     conn.createStatement().use {
                         val preparedStatement = conn.prepareStatement(newsInsertStatement)
                         preparedStatement.executeUpdate()
--- components/data-analyzer/src/test/kotlin/test/goodboards/app/analyzer/AnalyzerWorkerTest.kt
+++ components/data-analyzer/src/test/kotlin/test/goodboards/app/analyzer/AnalyzerWorkerTest.kt
@@ -1,17 +1,24 @@
 package test.goodboards.app.analyzer
 
-import com.goodboards.app.analyzer.DatabaseInit
+import com.goodboards.app.analyzer.NewsUnit
+import com.goodboards.app.analyzer.Wrapper
+import com.goodboards.db.DBConnection
+import com.goodboards.db.DBInterface
 import com.goodboards.db.DriverManagerWrapper
 import com.goodboards.db.SystemWrapper
 import com.goodboards.redis.RedisInterface
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
+import io.mockk.verify
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import org.junit.Test
 import kotlinx.serialization.json.Json
+import org.junit.Before
 import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 import kotlin.test.assertEquals
 
 
@@ -20,5 +27,123 @@ import kotlin.test.assertEquals
 data class NewsUnit(val gameID: String, var title: String, var description: String, val url: String)
 
 class AnalyzerWorkerTest{
+    @Before
+    fun mockDBConnection(): Unit {
+        mockkObject(SystemWrapper)
+        every { SystemWrapper.getenv("JDBC_DATABASE_URL") } returns "fakeURL"
+        every { SystemWrapper.getenv("DATABASE_USERNAME") } returns "fakeUsername"
+        every { SystemWrapper.getenv("DATABASE_PASSWORD") } returns "fakePassword"
+        val mockedConnection: Connection = mockk(relaxed = true)
+        mockkObject(DriverManagerWrapper)
+        every {
+            DriverManagerWrapper.getConnection(
+                "fakeURL",
+                "fakeUsername",
+                "fakePassword"
+            )
+        } returns mockedConnection
+    }
+    @Test
+    fun testDBConnection(){
+        val credentials = DBConnection.getCredentials()
+        assertEquals("fakeURL", credentials.url)
+        assertEquals("fakeUsername", credentials.username)
+        assertEquals("fakePassword", credentials.password)
+
+    }
+
+//    fun allmocks(){
+//        val mockedWrapper: Wrapper = mockk(relaxed = true)
+//        val mockedRedisInterface: RedisInterface = mockk(relaxed = true)
+//        val mockedDBConnection: Connection = mockk(relaxed = true)
+//        every { mockedWrapper.getTableName() } returns "FakeGoodboards.news"
+//        every { mockedWrapper.getRedisInterface() } returns mockedRedisInterface
+//        every { mockedWrapper.getDBConnection()} returns mockedDBConnection
+//        every { mockedWrapper.getInsertNewsStatement("FakeUUID1", NewsUnit("Fake gameId", "Fake title", "Fake description", "Fake URL")) } returns "Fake Insert Statement"
+//        every { mockedWrapper.setDoubleApostrophe("Fake title") } returns "Fake title"
+//        every { mockedWrapper.setDoubleApostrophe("Fake description") } returns "Fake description"
+//        every { mockedWrapper.getRandomUUID()} returns "FakeUUID1"
+//        val newsUnits = arrayOf(
+//            """{
+//                "gameID": "Fake gameId",
+//                "title": "Fake title",
+//                "description": "Fake description",
+//                "url": "Fake URL"
+//            }""".trimIndent()
+//        )
+//        every { mockedRedisInterface.getFromList(key = "news:collector-analyze", 10) } returns newsUnits.toList()
+//    }
+
+    @Test
+    fun testRedisInteraction(){
+
+        val mockedWrapper: Wrapper = mockk(relaxed = true)
+        val mockedRedisInterface: RedisInterface = mockk(relaxed = true)
+        every { mockedWrapper.getRedisInterface() } returns mockedRedisInterface
+        val newsUnits = arrayOf(
+            """{
+                "gameID": "Fake gameId",
+                "title": "Fake title",
+                "description": "Fake description",
+                "url": "Fake URL"
+            }""".trimIndent()
+        )
+        every { mockedRedisInterface.getFromList(key = "news:collector-analyze", 10) } returns newsUnits.toList()
+
+        val redisInterface = mockedWrapper.getRedisInterface()
+        val redisQueue = redisInterface.getFromList(key = "news:collector-analyze", 10)
+        assert(redisQueue.size == 1)
+    }
+
+    @Test
+    fun testParsing(){
+        val mockedWrapper: Wrapper = mockk(relaxed = true)
+        every { mockedWrapper.setDoubleApostrophe("Fake title") } returns "Fake title"
+        every { mockedWrapper.setDoubleApostrophe("Fake description") } returns "Fake description"
+        val newsUnits = arrayOf(
+            """{
+                "gameID": "Fake gameId",
+                "title": "Fake title",
+                "description": "Fake description",
+                "url": "Fake URL"
+            }""".trimIndent()
+        )
+
+        val mockedList = newsUnits.toList()
+        val newsUnit = Json.decodeFromString<NewsUnit>(mockedList[0])
+        assertEquals("Fake URL", newsUnit.url)
+        assertEquals("Fake gameId", newsUnit.gameID)
+        assertEquals("Fake title", newsUnit.title)
+        assertEquals("Fake description", newsUnit.description)
+
+        assertEquals("Fake title", mockedWrapper.setDoubleApostrophe("Fake title"))
+        assertEquals("Fake description", mockedWrapper.setDoubleApostrophe("Fake description"))
+    }
+
+    @Test
+    fun testQuery(){
+        val mockedWrapper: Wrapper = mockk(relaxed = true)
+        val mockedRedisInterface: RedisInterface = mockk(relaxed = true)
+        val mockedDBConnection: Connection = mockk(relaxed = true)
+        every { mockedWrapper.getTableName() } returns "FakeGoodboards.news"
+        every { mockedWrapper.getRedisInterface() } returns mockedRedisInterface
+        every { mockedWrapper.getDBConnection()} returns mockedDBConnection
+        every { mockedWrapper.getInsertNewsStatement("FakeUUID1", NewsUnit("Fake gameId", "Fake title", "Fake description", "Fake URL")) } returns "Fake Insert Statement"
+        every { mockedWrapper.setDoubleApostrophe("Fake title") } returns "Fake title"
+        every { mockedWrapper.setDoubleApostrophe("Fake description") } returns "Fake description"
+        every { mockedWrapper.getRandomUUID()} returns "FakeUUID1"
+
+        val preparedStatement = mockk<PreparedStatement>()
+        val mockResultSet = mockk<ResultSet>()
+        val newsItem = NewsUnit("Fake gameId", "Fake title", "Fake description", "Fake URL")
+        every{ mockedDBConnection.prepareStatement("SELECT * FROM ${mockedWrapper.getTableName()} where title = '${newsItem.title}'")} returns preparedStatement
+
+        val mockedStatement = mockedDBConnection.prepareStatement("SELECT * FROM ${mockedWrapper.getTableName()} where title = '${newsItem.title}'")
+        every { mockedStatement.executeQuery() } returns mockResultSet
+
+        verify(exactly = 1) { preparedStatement.executeQuery() }
+
+
+    }
 
 }
