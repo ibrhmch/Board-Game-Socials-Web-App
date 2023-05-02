8abb7ee5e18463bfe7e54664de0d1c03d02aa87d
Lin Shi <linshi1768@gmail.com>
2023-05-01 23:54:24 -0600

    unit test for analyzer

--- components/data-analyzer/src/main/kotlin/com/goodboards/app/analyzer/AnalyzerWorker.kt
+++ components/data-analyzer/src/main/kotlin/com/goodboards/app/analyzer/AnalyzerWorker.kt
@@ -8,31 +8,25 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 
-class ExampleWorker(override val name: String = "data-analyzer") : Worker<AnalyzerTask> {
+class AnalyzerWorker(override val name: String = "data-analyzer") : Worker<AnalyzerTask> {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     override fun execute(task: AnalyzerTask) {
         runBlocking {
             logger.info("starting data analysis.")
 
-            // todo - data analysis happens here
-            val conn = Wrapper.getDBConnection()
+            val dbInterface = Wrapper.getDBInterface()
+            val conn = Wrapper.getConnection()
             val redisInterface = Wrapper.getRedisInterface()
             val redisQueue = redisInterface.getFromList(key = "news:collect-analyze", 10)
             for (item in redisQueue) {
-                var newsItem = Json.decodeFromString<NewsUnit>(item)
-                newsItem.title = Wrapper.setDoubleApostrophe(newsItem.title)
-                newsItem.description = Wrapper.setDoubleApostrophe(newsItem.description)
-                val query =
-                    conn.prepareStatement("SELECT * FROM ${Wrapper.getTableName()} where title = '${newsItem.title}';")
-                val result = query.executeQuery()
-                if (!result.next()) {
-                    val newsInsertStatement =
-                        Wrapper.getInsertNewsStatement(Wrapper.getRandomUUID(), newsItem)
-                    conn.createStatement().use {
-                        val preparedStatement = conn.prepareStatement(newsInsertStatement)
-                        preparedStatement.executeUpdate()
-                        preparedStatement.close()
+                val newsItem = Json.decodeFromString<NewsUnit>(item)
+                newsItem.title = AnalyzerWorkerHelper.setDoubleApostrophe(newsItem.title)
+                newsItem.description = AnalyzerWorkerHelper.setDoubleApostrophe(newsItem.description)
+                val result = AnalyzerWorkerHelper.getQuery(newsItem, conn)
+                if (result != null) {
+                    if (!result.next()) {
+                        dbInterface.addNews(newsItem.title,newsItem.description,newsItem.url, newsItem.gameID, Wrapper.getRandomUUID())
                     }
                 }
                 logger.info("completed data analysis.")
--- components/data-analyzer/src/main/kotlin/com/goodboards/app/analyzer/Wrapper.kt
+++ components/data-analyzer/src/main/kotlin/com/goodboards/app/analyzer/Wrapper.kt
@@ -1,6 +1,7 @@
 package com.goodboards.app.analyzer
 
 import com.goodboards.db.DBConnection
+import com.goodboards.db.DBInterface
 import com.goodboards.redis.RedisInterface
 import java.sql.Connection
 import java.sql.DriverManager
@@ -12,29 +13,14 @@ object Wrapper {
 
     fun getRedisInterface(): RedisInterface = RedisInterface()
 
-    fun getTableName(): String = NEWS_TABLE_NAME
-
-    fun getDBConnection(): Connection {
+    fun getConnection(): Connection {
         val dbCredentials = DBConnection.getCredentials()
         return DriverManager.getConnection(dbCredentials.url, dbCredentials.username, dbCredentials.password)
     }
 
-    fun getInsertNewsStatement(id: String, news: NewsUnit): String{
-        return "INSERT INTO ${NEWS_TABLE_NAME}(id, gameId, title, description, url) VALUES ('${id}', '${news.gameID}', '${news.title}', '${news.description}', '${news.url}');"
+    fun getDBInterface(): DBInterface{
+        return DBInterface(DBConnection)
     }
-
-    fun setDoubleApostrophe(str: String): String{
-        var temp = ""
-        for(character in str.iterator()){
-            if(character == '\''){
-                temp = temp + character.toString() + character.toString()
-            }else {
-                temp += character.toString()
-            }
-        }
-        return temp
-    }
-
-    fun getRandomUUID(): String = UUID.randomUUID().toString()!!
+    fun getRandomUUID(): String = UUID.randomUUID().toString()
 
 }
