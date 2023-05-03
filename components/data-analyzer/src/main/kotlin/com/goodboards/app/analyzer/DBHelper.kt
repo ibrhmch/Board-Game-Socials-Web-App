4f5bb97752390e9372e5077edeb39ee1d9022a7e
Lin Shi <linshi1768@gmail.com>
2023-05-02 21:31:18 -0600

    dbhelper copied to analyzer

---
+++ components/data-analyzer/src/main/kotlin/com/goodboards/app/analyzer/DBHelper.kt
@@ -0,0 +1,34 @@
+package com.goodboards.app.analyzer
+
+import com.goodboards.db.DBConnection
+import com.goodboards.db.DBInterface
+import com.goodboards.db.SystemWrapper
+
+// TODO refactor DB Connection management
+object DBHelper {
+
+        var dbInterface : DBInterface? = null
+        data class DatabaseCredential(val url: String, val username: String, val password: String)
+
+        private fun initDBInterface() : DBInterface{
+            val dbCredentials = getDatabaseCredentials()
+            val dbConnection = DBConnection
+            dbConnection.setConnection(dbCredentials.url, dbCredentials.username, dbCredentials.password)
+            return DBInterface(dbConnection)
+        }
+
+        fun getDBInterface() : DBInterface {
+            if (dbInterface == null) {
+                dbInterface = initDBInterface()
+            }
+            return dbInterface!!
+        }
+
+        fun getDatabaseCredentials() : DatabaseCredential {
+            val DB_URL: String = SystemWrapper.getenv("JDBC_DATABASE_URL")
+            val DB_USENAME : String =  SystemWrapper.getenv("DATABASE_USERNAME")
+            val DB_PASSWORD : String = SystemWrapper.getenv("DATABASE_PASSWORD")
+            return DatabaseCredential(DB_URL, DB_USENAME, DB_PASSWORD)
+        }
+
+}
